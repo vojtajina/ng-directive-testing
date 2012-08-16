3789dedd04d09d19312019dedcb1bcd0e1d78595
Vojta Jina <vojta.jina@gmail.com>
2012-08-15 17:33:40 -0700

    Simple tabs directive from homepage

---
+++ app.js
@@ -0,0 +1,20 @@
+var app = angular.module('app', ['tabs']);
+
+
+app.controller('BeerCounter', function($scope, $locale) {
+  $scope.beers = [0, 1, 2, 3, 4, 5, 6];
+  if ($locale.id == 'en-us') {
+    $scope.beerForms = {
+      0: 'no beers',
+      one: '{} beer',
+      other: '{} beers'
+    };
+  } else {
+    $scope.beerForms = {
+      0: 'žiadne pivo',
+      one: '{} pivo',
+      few: '{} pivá',
+      other: '{} pív'
+    };
+  }
+});
---
+++ tabs.js
@@ -0,0 +1,42 @@
+var tabs = angular.module('tabs', []);
+
+
+tabs.directive('tabs', function() {
+  return {
+    restrict: 'E',
+    transclude: true,
+    scope: {},
+    controller: function($scope, $element) {
+      var panes = $scope.panes = [];
+
+      $scope.select = function(pane) {
+        angular.forEach(panes, function(pane) {
+          pane.selected = false;
+        });
+        pane.selected = true;
+      }
+
+      this.addPane = function(pane) {
+        if (panes.length == 0) $scope.select(pane);
+        panes.push(pane);
+      }
+    },
+    templateUrl: 'tpl/tabs.html',
+    replace: true
+  };
+});
+
+
+tabs.directive('pane', function() {
+  return {
+    require: '^tabs',
+    restrict: 'E',
+    transclude: true,
+    scope: { title: '@' },
+    link: function(scope, element, attrs, tabsCtrl) {
+      tabsCtrl.addPane(scope);
+    },
+    templateUrl: 'tpl/pane.html',
+    replace: true
+  };
+});
